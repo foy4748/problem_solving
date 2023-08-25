880d9629a15942d0b1d696426f9637fd8c9384d0
foy4748 <faisaljfcl@gmail.com>
2023-08-26 01:18:57 +0600

    Solved chunkArray

--- js30/chunkArray.js
+++ js30/chunkArray.js
@@ -1,10 +1,10 @@
 var chunk = function (arr, size) {
-	let newArr = []
-	let numOfChunks = Math.ceil(arr.length / size);
-	for (let i = 0; i < numOfChunks; i++) {
-		newArr.push(arr.slice(i * size, i + size))
-	}
-	return newArr;
+  let newArr = [];
+  let numOfChunks = Math.ceil(arr.length / size);
+  for (let i = 0; i < numOfChunks; i++) {
+    newArr.push(arr.slice(i * size, i * size + size));
+  }
+  return newArr;
 };
 
-console.log(chunk([1, 9, 6, 3, 2], 3))
+console.log(chunk([1, 9, 6, 2], 3));
