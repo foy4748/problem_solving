9df6e2624a4f9e4f72f416c45e61234fdb39f692
foy4748 <faisaljfcl@gmail.com>
2023-10-10 13:54:18 +0600

    Solved the Pattern problem in 'Conditions & Loop' section

--- hackerRank/C/Conditionals_and_Loop/printPatterns.c
+++ hackerRank/C/Conditionals_and_Loop/printPatterns.c
@@ -3,18 +3,41 @@
 #include <math.h>
 #include <stdlib.h>
 
+void printGrid(int size, int grid[2*size-1][2*size-1]){
+	for(int i=0; i<2*size-1; i++){
+		for(int j = 0; j<2*size-1; j++){
+			printf("%d ",grid[i][j]);
+		}
+		printf("\n");
+	}
+}
+
 int main() 
 {
 
     int n;
     scanf("%d", &n);
-	int i,j; // Iterators
+	int grid[2*n-1][2*n-1];
+	int i,j,k; // Iterators
   	// Complete the code to print the pattern.
-	for(i=1; i<= 2*n-1; i++){
-		for(j=1; j<= 2*n-1; j++){
-			printf("%d\t", n - i + 1);
+	
+	// Constructing grid
+	for(i=0; i< 2*n-1; i++){
+		for(j=0; j< 2*n-1; j++){
+			grid[i][j] = n;
 		}
-		printf("\n");
 	}
+
+	// Printing Pattern
+	for(k=1; k<n; k++){
+		for(i=k; i<(2*n-1)-k; i++){
+			for(j=k; j<(2*n-1)-k; j++){
+				grid[i][j] = grid[i][j] - 1;
+			}
+		}
+
+	}
+	
+	printGrid(n,grid);
     return 0;
 }
